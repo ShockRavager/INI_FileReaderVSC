cd580e668c7c65faec8ed2b4c7139cd3ff59080c
Shock Ravager <pleiadi17@gmail.com>
2022-11-16 11:26:25 +0100

    Test

--- Launch.cpp
+++ Launch.cpp
@@ -3,7 +3,21 @@
 // PATH     ->  *
 // FILE     ->  Launch.cpp
 
+#include "Content/Objects/SaveData/INI_File.hpp"
+
 
 int main() {
+    INI_File LFile;
+
+    LFile.AddSection("Section1");
+    LFile.AddSection("Section2");
+    LFile.AddParam("Section1", "Param1");
+    LFile.AddParam("Section1", "Param2");
+    LFile.AddParam("Section1", "Param3");
+    LFile.AddParam("Section1", "Param4");
+    LFile.AddParam("Section2", "Param1");
+    LFile.AssignParam("Section1", "Param1", "100");
+    LFile.AssignParam("Section1", "Param2", "150");
+    LFile.SaveToFile("Config", "Data/INI");
     return 0;
 }
