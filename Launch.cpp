f6d27be6e0850a75cadb454975b023ea42f992df
Shock Ravager <pleiadi17@gmail.com>
2022-11-16 15:47:33 +0100

    Update GLOBAL

--- Launch.cpp
+++ Launch.cpp
@@ -5,9 +5,17 @@
 #include "Content/Objects/SaveData/INI_File.hpp"
 #include "UnitTest/INI_FileUT.hpp"
 
+using namespace std;
+
 
 int main() {
-    INI_FileUT::SaveData();
-    INI_FileUT::AssignValue();
+    INI_File LFile;
+
+    LFile.AddSection("Section1");
+    LFile.AddParam("Section1", "Param1");
+    LFile.AddParam("Section1", "Param3");
+    LFile.LoadFromFile("Config", "Data/INI/");
+    cout << "\n";
+    LFile.PrintRows();
     return 0;
 }
